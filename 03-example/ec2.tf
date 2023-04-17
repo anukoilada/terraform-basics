13e3bce6a5e09d2924f7004167745d54040ab26b
anupama <anupama@gmail.com>
2023-04-17 17:05:37 -0400

    Git Auto Push

--- 03-example/ec2.tf
+++ 03-example/ec2.tf
@@ -12,3 +12,31 @@ resource "aws_instance" "web" {
 output "private_dns_of_server" {
     value       = aws_instance.web.private_dns
 }
+
+# Creatting a security group instamce
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  tags = {
+    Name = "allow_SSH"
+  }
+}
