437d31e5f1dfae604b73279b1a4a2e141f5037d8
anupama <anupama@gmail.com>
2023-04-17 17:24:01 -0400

    Git Auto Push

--- 03-example/ec2.tf
+++ 03-example/ec2.tf
@@ -2,6 +2,7 @@
 resource "aws_instance" "web" {
   ami                       = "ami-06ef3429338c1779f"
   instance_type             = "t3.nano"
+  vpc_security_group_ids    = [aws_security_group.allow_ssh_sg.id]
 
   tags = {
     Name        = "Terraform-Instance-Server-Name"
